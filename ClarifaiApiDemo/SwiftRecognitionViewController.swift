a98542d7b3a70fcbb1280519ce543f8363cb7b60
Keith Ito <kito@clarifai.com>
2015-10-01 22:08:31 -0400

    Use more conventional Swift style.
    Fix issue with ClarifaiClient creation.

--- ClarifaiApiDemo/SwiftRecognitionViewController.swift
+++ ClarifaiApiDemo/SwiftRecognitionViewController.swift
@@ -9,33 +9,28 @@ import UIKit
  * This view controller performs recognition using the Clarifai API.
  */
 class SwiftRecognitionViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // IMPORTANT NOTE: you should replace these keys with your own App ID and secret.
+    // These can be obtained at https://developer.clarifai.com/applications
+    static let appID = "vM05qo55uhZard2dL4BixmMm4WsHIl6CsGCTgS_7"
+    static let appSecret = "rx4oPPiXiCWNRVcoJ0huLz02cKiQUZtq5JPVrhjM"
 
-    private struct Constants {
-        // IMPORTANT NOTE: you should replace these keys with your own App ID and secret.
-        // These can be obtained at https://developer.clarifai.com/applications
-        static let AppID = "vM05qo55uhZard2dL4BixmMm4WsHIl6CsGCTgS_7"
-        static let AppSecret = "rx4oPPiXiCWNRVcoJ0huLz02cKiQUZtq5JPVrhjM"
-
-        // Custom Training (Alpha): to predict against a custom concept (instead of the standard
-        // tag model), set this to be the name of the concept you wish to predict against. You must
-        // have previously trained this concept using the same app ID and secret as above. For more
-        // info on custom training, see https://github.com/Clarifai/hackathon
-        static let ConceptName: String? = nil
-        static let ConceptNamespace = "default"
-    }
+    // Custom Training (Alpha): to predict against a custom concept (instead of the standard
+    // tag model), set this to be the name of the concept you wish to predict against. You must
+    // have previously trained this concept using the same app ID and secret as above. For more
+    // info on custom training, see https://github.com/Clarifai/hackathon
+    static let conceptName: String? = nil
+    static let conceptNamespace = "default"
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var button: UIButton!
 
-    private var client : ClarifaiClient {
-        get {
-            let c = ClarifaiClient(appID: Constants.AppID, appSecret: Constants.AppSecret)
-            // Uncomment this to request embeddings. Contact us to enable embeddings for your app:
-            // c.enableEmbed = true
-            return c
-        }
-    }
+    private lazy var client : ClarifaiClient = {
+        let c = ClarifaiClient(appID: appID, appSecret: appSecret)
+        // Uncomment this to request embeddings. Contact us to enable embeddings for your app:
+        // c.enableEmbed = true
+        return c
+    }()
 
     @IBAction func buttonPressed(sender: UIButton) {
         // Show a UIImagePickerController to let the user pick an image from their library.
@@ -73,28 +68,30 @@ class SwiftRecognitionViewController : UIViewController, UIImagePickerController
         // Encode as a JPEG.
         let jpeg = UIImageJPEGRepresentation(scaledImage, 0.9)!
 
-        if (Constants.ConceptName == nil) {
+        if SwiftRecognitionViewController.conceptName == nil {
             // Standard Recognition: Send the JPEG to Clarifai for standard image tagging.
-            client.recognizeJpegs([jpeg], completion: { (results: [ClarifaiResult]?, error: NSError?) in
-                if (error != nil) {
+            client.recognizeJpegs([jpeg]) {
+                (results: [ClarifaiResult]?, error: NSError?) in
+                if error != nil {
                     print("Error: \(error)\n")
                     self.textView.text = "Sorry, there was an error recognizing your image."
                 } else {
                     self.textView.text = "Tags:\n" + results![0].tags.joinWithSeparator(", ")
                 }
                 self.button.enabled = true
-            })
+            }
         } else {
             // Custom Training: Send the JPEG to Clarifai for prediction against a custom model.
-            client.predictJpegs([jpeg], conceptNamespace: Constants.ConceptNamespace, conceptName: Constants.ConceptName, completion: { (results: [ClarifaiPredictionResult]?, error: NSError?) in
-                if (error != nil) {
+            client.predictJpegs([jpeg], conceptNamespace: SwiftRecognitionViewController.conceptNamespace, conceptName: SwiftRecognitionViewController.conceptName) {
+                (results: [ClarifaiPredictionResult]?, error: NSError?) in
+                if error != nil {
                     print("Error: \(error)\n")
                     self.textView.text = "Sorry, there was an error running prediction on your image."
                 } else {
-                    self.textView.text = "Prediction score for \(Constants.ConceptName!):\n\(results![0].score)"
+                    self.textView.text = "Prediction score for \(SwiftRecognitionViewController.conceptName!):\n\(results![0].score)"
                 }
                 self.button.enabled = true
-            })
+            }
         }
     }
 }
